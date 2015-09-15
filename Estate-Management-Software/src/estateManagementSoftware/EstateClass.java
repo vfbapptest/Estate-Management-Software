e296d2eb9777bdcd9cc0e2df3ff1e8b79d59c5df
vfbapptest <vfbapptest@gmail.com>
2015-09-15 12:34:39 -0400

    Initial Commit

---
+++ Estate-Management-Software/src/estateManagementSoftware/EstateClass.java
@@ -0,0 +1,11 @@
+package estateManagementSoftware;
+
+
+public class EstateClass {
+
+	public static void main(String[] args) {
+		System.out.println("This is the estate management software project");
+
+	}
+
+}
